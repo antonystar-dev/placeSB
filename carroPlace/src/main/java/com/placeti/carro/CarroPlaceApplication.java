baca508108c8a0de5883af9c12221b6ed9eb640e
antonystar-dev <antonioamuniz@outlook.com>
2023-09-21 23:20:24 -0300

    Criação do projeto em Springboot e suas dependencias

---
+++ carroPlace/src/main/java/com/placeti/carro/CarroPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.placeti.carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroPlaceApplication.class, args);
+	}
+
+}
---
+++ carroPlace/src/test/java/com/placeti/carro/CarroPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placeti.carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
