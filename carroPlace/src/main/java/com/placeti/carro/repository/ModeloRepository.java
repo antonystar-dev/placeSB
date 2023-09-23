37201206ecfbfbf78d7aa6d47d1673bbd250e84a
antonystar-dev <antonioamuniz@outlook.com>
2023-09-22 22:20:27 -0300

    Criação do PUT, Delete, Post e Get do Modelo

--- carroPlace/src/main/java/com/placeti/carro/repository/ModeloRepository.java
+++ carroPlace/src/main/java/com/placeti/carro/repository/ModeloRepository.java
@@ -1,5 +1,10 @@
 package com.placeti.carro.repository;
 
-public interface ModeloRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.placeti.carro.model.Modelo;
+@Repository
+
+public interface ModeloRepository extends JpaRepository<Modelo, Long>{
 
 }
